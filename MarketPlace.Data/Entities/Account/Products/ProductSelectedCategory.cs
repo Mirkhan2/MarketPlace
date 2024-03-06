c8a7644ded53820c5c41aeab53c66b7470a89d44
Mirkhan2 <mirkhan.shams2@gmail.com>
2024-03-06 21:48:07 +0100

    Product Updates

--- MarketPlace.Data/Entities/Account/Products/ProductSelectedCategory.cs
+++ MarketPlace.Data/Entities/Account/Products/ProductSelectedCategory.cs
@@ -10,12 +10,18 @@ namespace MarketPlace.Data.Entities.Account.Products
     public class ProductSelectedCategory : BaseEntity
     {
         #region properties
+
         public long ProductId { get; set; }
+
         public long ProductCategoryId { get; set; }
+
         #endregion
+
+        #region relations
+
         public Product Product { get; set; }
+
         public ProductCategory ProductCategory { get; set; }
-        #region relation
 
         #endregion
     }
